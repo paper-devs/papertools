5d11663afa82af81a383347bfc3c333276903e7f
Kev <121695593+aiokev@users.noreply.github.com>
2024-02-10 02:37:31 +0100

    alpha

--- papertools/__init__.py
+++ papertools/__init__.py
@@ -21,7 +21,7 @@ if not discord.version_info.major >= 2:
         "using 'pip install discord.py'",
     )
 
-__version__ = "0.0.1"
+__version__ = "0.0.1a"
 __title__ = "papertools"
 __author__ = "aiokev"
 __license__ = "GPL-3.0"
