707a6dca94c7765d6f4265f5a5516a36c6242d38
Kev <121695593+aiokev@users.noreply.github.com>
2024-02-12 21:53:05 +0100

    Update __init__.py

--- papertools/__init__.py
+++ papertools/__init__.py
@@ -21,7 +21,7 @@ if not discordpy.version_info.major >= 2:
         "using 'pip install discord.py'",
     )
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __title__ = "papertools"
 __author__ = "aiokev"
 __license__ = "GPL-3.0"
